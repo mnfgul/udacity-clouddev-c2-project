780c09ed50460f8be95fbfa5ac20abc2accbe955
mnfgul <menafgul@gmail.com>
2019-07-27 15:17:09 +0300

    Filter image API endpoint

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
+
+    app.use("/filteredimage", async (req, res) => {
+        const {image_url} = req.query;
+
+        if(image_url){
+            try{
+								let filtered_image = await filterImageFromURL(image_url);
+								res.status(200).sendFile(filtered_image, async () =>{
+									await deleteLocalFiles([filtered_image]);
+								});
+            }catch(err){
+                res.status(402).send(err);
+            }
+        }else{
+            res.status(400).send('Image url is required')
+				}
+				
+    });
   
   // Root Endpoint
   // Displays a simple message to the user
